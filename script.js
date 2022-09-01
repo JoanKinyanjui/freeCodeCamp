da956deaa98cb743c7493d289519c58827dbe61e
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-09-01 21:01:44 +0300

    PROFILE LOOK UP

--- script.js
+++ script.js
@@ -92,3 +92,16 @@ function updateRecords(records, id, prop, value) {
     return records;
   }
   
+  //PROFILE LOOKUP
+  function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+      if (contacts[x].firstName === name) {
+        if (contacts[x].hasOwnProperty(prop)) {
+          return contacts[x][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
+  }
