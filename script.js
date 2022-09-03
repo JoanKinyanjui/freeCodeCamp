c47d85b544358821c694d2b776e5817a4c8b810c
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2022-09-03 12:25:46 +0300

    mutations

--- script.js
+++ script.js
@@ -272,3 +272,15 @@ console.log(students)
     }
     return updatedTitle.join(" ");
   }
+
+
+  // Mutations
+  function mutation(arr) {
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
+  
